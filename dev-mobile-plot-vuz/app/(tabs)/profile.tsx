f67cef7aaa678e61006450b5164f3fb27a39b136
Ctfy21 <skileev@bk.ru>
2024-10-28 18:59:08 +0300

    add photo picker

--- dev-mobile-plot-vuz/app/(tabs)/profile.tsx
+++ dev-mobile-plot-vuz/app/(tabs)/profile.tsx
@@ -1,12 +1,30 @@
 import { View, Text, TouchableOpacity, SafeAreaView, ScrollView, Image } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { Ionicons } from '@expo/vector-icons'
 import InfoBox from '@/components/InfoBox'
 import { useGlobalContext } from '@/context/GlobalProvider';
 import { signOut } from '@/scripts/api-config';
 import { router } from 'expo-router';
+import { savePhoto } from '@/scripts/save-photo';
+import * as ImagePicker from 'expo-image-picker';
 
 const Profile = () => {
+  const [image, setImage] = useState<string | null>(null);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 4],
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
 
   const { user, setUser, setIsLoggedIn } = useGlobalContext();
 
@@ -25,8 +43,11 @@ const Profile = () => {
             <Ionicons name="exit-outline" color="red" size={30}></Ionicons>
           </TouchableOpacity>
 
-          <View className='w-20 h-20 border border-black justify-center rounded-lg items-center'>
-            {/* <Image source={{ uri: user?.avatar }} className='w-[90%] h-[90%]' resizeMode='cover'></Image> */}
+          <View className='w-20 h-20 border bg-gray-300 border-black justify-center rounded-lg items-center'>
+            <TouchableOpacity onPress={pickImage} className='w-full items-center justify-center'>
+              <Image source={{ uri: image }} className='w-[90%] h-[90%] opacity-60' resizeMode='cover'></Image>
+              <Ionicons className="absolute align-middle" name="camera" size={30}></Ionicons>
+            </TouchableOpacity>
           </View>
 
           <InfoBox title={user?.username} subtitle="" containerStyles="mt-5" titleStyles="text-lg"/>
